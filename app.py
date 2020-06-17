b90b329db9ddeb6912541085a0e38fabf312a0d2
dealthy <55175568+dealthy@users.noreply.github.com>
2020-06-17 10:13:24 +0800

    v0

--- app.py
+++ app.py
@@ -9,20 +9,44 @@
 import random
 import os
 import time
-import sys
-import termios
-import contextlib
+from pynput import keyboard
+from pynput.keyboard import Key, Controller
 
-CRUCIALVARIABLES = [1000, 50, 60]
 #the words generated for each test
 #the words target for each word test
 #the time limit of each time test
 
+class CRUCIALVARIABLES():
+	genmaxword = 150
+	timelimit = 60
+	wordsinline = 10
+
+class param():
+	letter = []
+	keys = [" "]
+	endvalue = 0
+	endinginfo = [0,0]
+
+def on_press(key):
+	global param
+	try:
+		param.letter.append(key.char)
+	except AttributeError:
+		param.keys.append('{0}'.format(key))
+		if(key == keyboard.Key.esc):
+			param.endvalue = 1
+			return False
+		if(key == keyboard.Key.space and len(param.letter) != 0):
+			return False
+		if(key == keyboard.Key.backspace and len(param.letter) != 0):
+			del param.letter[-1]
+
 
 def wordRad():
+	global CRUCIALVARIABLES
 	#have the function that is dedicated to get words off from the txt file
 	words = []
-	for i in range(CRUCIALVARIABLES[0]):
+	for i in range(CRUCIALVARIABLES.genmaxword):
 		word = ""
 		temp = os.popen("shuf -n 1 words.txt").read()
 		for i in range(len(temp) - 1):
@@ -31,69 +55,90 @@ def wordRad():
 	return words
 #return time is too long
 
+
+def ui():
+	global param
+	global CRUCIALVARIABLES
+	#put good theme to make the text prinout in the center
+	os.system("clear")
+	while(param.endvalue == 0):	
+		param.letter = []
+		param.keys = [" "]
+		current = 0
+		correct = 0
+		starttime = 0
+		wordindex = 0
+		starttime = time.time()
+		words = wordRad()
+		while(time.time() - starttime <= CRUCIALVARIABLES.timelimit):
+			#output
+			resultReturn()
+			for i in range(CRUCIALVARIABLES.wordsinline):
+				if(wordindex + i == current):
+					print("\033[44;33m", words[wordindex + i] ,"\033[m", end = " ")
+				else:
+					print(words[wordindex + i], end = " ")
+			print(" ")
+			#input
+			wordinput = ""
+			while(wordinput == "" and param.keys[-1] != "Key.esc"):
+				with keyboard.Listener(on_press=on_press) as listener:
+					listener.join()
+				for l in range(len(param.letter)):
+					wordinput = wordinput + param.letter[l]
+			current += 1 
+			#var change
+			if(wordinput == words[current - 1]):
+				correct += 1
+			if(param.keys[-1] == "Key.space"):
+				param.letter = []
+			if(param.endvalue == 1):
+				break
+			if(current % CRUCIALVARIABLES.wordsinline == 0):
+				wordindex += CRUCIALVARIABLES.wordsinline
+			os.system("clear")
+		if(time.time() - starttime >= CRUCIALVARIABLES.timelimit):
+			param.endvalue = 1
+		else:
+			block = input("press enter button to continue...\n")
+			param.endvalue = 0
+		
+	param.endvalue = 0
+	param.endinginfo = [correct, current]
+
 #endinginfo[0] == wordtyped
 #endinginfo[1] == wordcompleted
 #endinginfo[2] == time used(s))
 
-def gui(words, testtype):
-	#put good theme to make the text prinout in the center
+def resultReturn():
+	global CRUCIALVARIABLES
+	global param
 	os.system("clear")
-	if(testtype % 2 == 0):
-		#place limits for time
-		print("time")
-	else:
-		current = 0
-		correct = 0
-		starttime = time.time()
-		for i in range(0, CRUCIALVARIABLES[1], 5):
-			for j in range(5):
-				#output
-				for k in range(5):
-					wordindex = i + k
-					if(wordindex == current):
-						print("\033[44;33m", words[wordindex] ,"\033[m", end = " ")
-					else:
-						print(words[wordindex], end = " ")
-				print(" ")
-				#print(wordindex,  current, correct, i)
-				#switch to event listener
-				#using pynput
-				wordinput = input("input: ")
-				#need to fix: terminates each input by space instead of enter
-				if(wordinput == words[current]):
-					correct += 1
-				current += 1
-				os.system("clear")
-		endinginfo = [CRUCIALVARIABLES[1], correct, int(time.time() - starttime)]
-	return endinginfo
-
-def resultReturn(endinginfo):
-	wpm = int(endinginfo[1] / endinginfo[2] * 60)
-	if(wpm / 100 <= 0):
-		wpm = wpm + " "
-	acc = int(endinginfo[1] / endinginfo[0] * 100)
-	if(acc / 100 <= 0):
-		acc = acc + " "
-	print(" ==============\n", "| wpm = ", wpm, " |\n", "| acc = ", acc, " |\n", "==============\n\n\n")
+	if(param.endinginfo != [0,0]):
+		wpm = str(int(param.endinginfo[0] / (CRUCIALVARIABLES.timelimit / 60)))
+		acc = str(int(param.endinginfo[0] / param.endinginfo[1] * 100))
+	else: 
+		wpm = 0
+		acc = 0
+	print("\n =============\n", "| wpm = ", wpm, " |\n", "| acc = ", acc, " |\n", "=============\n")
 	#print out results
 
+os.system("clear")
 print("welcome to typing practice...")
-print("you can either set 60 seconds or 50 words as your target")
-print("60 seconds test is set to default")
-status = 1
+print("u r aiming 2 type as much as u can in 60s")
+parameters = param()
+variables = CRUCIALVARIABLES()
 
 while True:
-	resultReturn(gui(wordRad(), status))
-	choice = input("quit or continue: \nuse q to quit,\nuse s to switch modes,\nother keys to continue...\n")
-	if(choice == "q"):
+	ui()
+	resultReturn()
+	choice = input("quit or continue: \npress q + enter to quit,\nenter key to continue...\n")
+	if(choice[-1] == "q"):
 		print("thank you for using...")
-		os.system('clear')
 		break
-	if(choice == "s"):
-		status += 1
-		os.system('clear')
-	else:
-		continue
+
+os.system("clear")
+		
 
 
 
